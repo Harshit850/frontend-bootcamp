b304624080431a705331362be19cbc31f3cbd6dd
Harshit Kumar <kumarharshit850@gmail.com>
2021-07-02 21:01:13 +0530

    home page done

---
+++ URBAN-CLAB-CLONE/script.js
@@ -0,0 +1,21 @@
+// var links = document.querySelectorAll('.nav-item');
+
+// var [first, second, third] = links;
+
+// first.addEventListener('click', () => {
+//   second.classList.remove('active');
+//   third.classList.remove('active');
+//   first.classList.add('active');
+// });
+
+// second.addEventListener('click', () => {
+//   first.classList.remove('active');
+//   third.classList.remove('active');
+//   second.classList.add('active');
+// });
+
+// third.addEventListener('click', () => {
+//   first.classList.remove('active');
+//   second.classList.remove('active');
+//   third.classList.add('active');
+// });
